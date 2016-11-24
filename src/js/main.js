4f4eb2b84cb944334808d463faa5f1ead2da1c38
Audrey Carlsen <Audrey@Audreys-MacBook-Air.local>
2016-11-23 17:14:23 -0800

    adds pronunciations

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,12 @@
 // var track = require("./lib/tracking");
 
 require("component-responsive-frame/child");
+
+var $ = require("./lib/qsa");
+
+$(".listen").forEach(function(l) {
+	l.addEventListener("click", function(e) {
+		var id = e.target.getAttribute("data-word");
+		document.querySelector("#" + id).play();
+	});
+});
